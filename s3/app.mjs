68f4955353031b7cbe275ca13b86870c237a4f20
“NischithIsAFK” <nischith.212@gmail.com>
2024-09-24 20:22:21 +0530

    added logic to the code

--- s3/app.mjs
+++ s3/app.mjs
@@ -1,13 +1,44 @@
-export const lambdaHandler = async (event, context) => {
+const AWS = require("aws-sdk");
+const ses = new AWS.SES();
+
+exports.handler = async (event) => {
+  const bucketName = process.env.BUCKET_NAME;
+  const destEmail = process.env.DEST_EMAIL;
+
+  // Extract object information from the event
+  const records = event.Records;
+  const deletedFiles = records.map((record) => record.s3.object.key);
+
+  // Email subject and body
+  const emailSubject = `Object Deleted from S3 Bucket: ${bucketName}`;
+  const emailBody =
+    `The list of objects were deleted in bucket ${bucketName} with the prefix /img are:\n\n` +
+    deletedFiles.join("\n");
+
+  // SES Email parameters
+  const params = {
+    Destination: {
+      ToAddresses: [destEmail],
+    },
+    Message: {
+      Body: {
+        Text: { Data: emailBody },
+      },
+      Subject: { Data: emailSubject },
+    },
+    Source: destEmail,
+  };
+
   try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "hello world",
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-    return err;
+    await ses.sendEmail(params).promise();
+    console.log("Email sent successfully.");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Failed to send email");
   }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Email sent successfully" }),
+  };
 };
